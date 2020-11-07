fc371ee0dde927484b6f1723ffe7e36b3a0719ce
Hilary Valencia-Walsh <hilaryvalenciawalsh@gmail.com>
2020-11-07 04:03:11 -0800

    links 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app.js';
-import * as serviceWorker from './serviceWorker.js';
+import App from 'app.js';
+import * as serviceWorker from 'serviceWorker.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
